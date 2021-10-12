1389c8c046fca8b3413179201c35af1d576efb8b
杨鸿 <iyanghong@foxmail.com>
2021-10-12 21:37:31 +0800

    新增颜色布局
    Signed-off-by: 杨鸿 <iyanghong@foxmail.com>

---
+++ examples/components/color.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="color">
+      <span>主要色调</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "color",
+  data() {
+    return {}
+  },
+  created() {
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
