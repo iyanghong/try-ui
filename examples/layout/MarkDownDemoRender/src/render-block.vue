7f7af67c5f0a70421c65ed4b39bc8a316dae4f4d
iyanghong <iyanghong@foxmail.com>
2021-10-13 23:34:51 +0800

    修改demo-block布局

--- examples/layout/MarkDownDemoRender/src/render-block.vue
+++ examples/layout/MarkDownDemoRender/src/render-block.vue
@@ -2,23 +2,17 @@
 	<div :class="['demo-block',blockClass]">
 		<div class="demo-block-source">
 			<slot name="source"></slot>
-			<span class="demo-block-code-icon"
-				  v-if="!$slots.default"
-				  @click="showCode=!showCode">
-        <img alt="expand code" src="https://gw.alipayobjects.com/zos/rmsportal/wSAkBuJFbdxsosKKpqyq.svg"
-			 class="code-expand-icon-show"></span>
 		</div>
-		<div class="demo-block-meta"
-			 v-if="$slots.default">
-			<slot></slot>
-			<span v-if="$slots.default"
-				  class="demo-block-code-icon"
-				  @click="showCode=!showCode">
+		<div class="demo-block-control">
+			<span class="demo-block-code-icon" @click="showCode=!showCode">
         <img alt="expand code" src="https://gw.alipayobjects.com/zos/rmsportal/wSAkBuJFbdxsosKKpqyq.svg"
 			 class="code-expand-icon-show"></span>
 		</div>
 		<div class="demo-block-code"
 			 v-show="showCode">
+			<div class="demo-block-description" v-if="$slots.default">
+				<slot></slot>
+			</div>
 			<slot name="highlight"></slot>
 		</div>
 	</div>
@@ -51,20 +45,24 @@ export default {
 	display: block;
 	width: calc(100% - 32px);
 	position: relative;
-	margin: 16px;
+	margin: 16px 16px 16px 0;
 	-webkit-transition: all 0.2s;
 	transition: all 0.2s;
 	box-sizing: border-box;
+
 	&.hover {
 		box-shadow: 0 0 8px 0 rgba(232, 237, 250, .6), 0 2px 4px 0 rgba(232, 237, 250, .5);
 	}
+
 	.demo-button {
 		float: right;
 	}
+
 	p {
 		padding: 0;
 		margin: 0;
 	}
+
 
 	.demo-block-code-icon {
 		position: absolute;
@@ -131,18 +129,59 @@ export default {
 			font-size: 14px;
 		}
 	}
+	.demo-block-control{
+		border-top: solid 1px #eaeefb;
+		height: 44px;
+		box-sizing: border-box;
+		background-color: #fff;
+		border-bottom-left-radius: 4px;
+		border-bottom-right-radius: 4px;
+		text-align: center;
+		margin-top: -1px;
+		color: #d3dce6;
+		cursor: pointer;
+		position: relative;
+	}
 
 	.demo-block-code {
 		background-color: #f7f7f7;
 		font-size: 0;
+		overflow: auto;
+		.demo-block-description {
+			padding: 12px;
+			box-sizing: border-box;
+			border: solid 1px #ebebeb;
+			border-radius: 3px;
+			font-size: 14px;
+			line-height: 22px;
+			color: #666;
+			word-break: break-word;
+			margin: 20px 20px 0 20px;
+			background-color: #fff;
 
+			p {
+				margin: 0;
+				line-height: 26px;
+			}
+
+			code {
+				color: #5e6d82;
+				background-color: #e6effb;
+				margin: 0 4px;
+				display: inline-block;
+				padding: 1px 5px;
+				font-size: 12px;
+				border-radius: 3px;
+				height: 18px;
+				line-height: 18px;
+			}
+		}
 		code {
 			background-color: #f7f7f7;
 			font-family: Consolas, Menlo, Courier, monospace;
 			border: none;
 			display: block;
 			font-size: 14px;
-			padding: 16px 32px;
 
 			pre {
 				margin: 0;
