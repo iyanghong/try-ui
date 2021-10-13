50c69bd886f777aa3d8acf0e4d506bf374c44dce
iyanghong <iyanghong@foxmail.com>
2021-10-13 23:45:09 +0800

    修复NProgress不正常显示

--- examples/layout/header.vue
+++ examples/layout/header.vue
@@ -1,36 +1,37 @@
 <template>
-  <div class="main-header">
-    <div class="header-left">
-      <div class="logo"><span>Try UI</span></div>
-      <div class="header-nav">
-        <div class="header-nav-item"><a href="">首页</a></div>
-        <div class="header-nav-item"><a :class="{'active':routePath.indexOf('/components') === 0}" href="">组件</a></div>
-      </div>
-      <search></search>
-    </div>
+	<div class="main-header">
+		<div class="header-left">
+			<div class="logo"><span>Try UI</span></div>
+			<div class="header-nav">
+				<div class="header-nav-item"><a href="">首页</a></div>
+				<div class="header-nav-item"><a :class="{'active':routePath.indexOf('/components') === 0}"
+												href="">组件</a></div>
+			</div>
+			<search></search>
+		</div>
 
 
-    <div class="header-right header-nav">
-      <div class="header-nav-item"><a href="">English</a></div>
-      <div class="header-nav-item"><a href="">主题</a></div>
-      <div class="header-nav-item"><a target="_blank" href="https://github.com/iyanghong/try-ui">GitHub</a></div>
-    </div>
-  </div>
+		<div class="header-right header-nav">
+			<div class="header-nav-item"><a href="">English</a></div>
+			<div class="header-nav-item"><a href="">主题</a></div>
+			<div class="header-nav-item"><a target="_blank" href="https://github.com/iyanghong/try-ui">GitHub</a></div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "MainHeader",
-  data() {
-    return {}
-  },
-  computed: {
-    routePath() {
-      return this.$route.path;
-    }
-  },
-  created() {
-  }
+	name: "MainHeader",
+	data() {
+		return {}
+	},
+	computed: {
+		routePath() {
+			return this.$route.path;
+		}
+	},
+	created() {
+	}
 }
 </script>
 
@@ -38,50 +39,51 @@ export default {
 @import "src/theme/mixins/mixins";
 
 .main-header {
-  @mixin headerFlex{
-    display: flex;
-    flex-wrap: nowrap;
-    align-content: center;
-    align-items: center;
-  }
-  height: 60px;
-  position: fixed;
-  left: 0;
-  right: 0;
-  border-bottom: 1px solid $border-color;
-  @include headerFlex;
-  color: $text-color;
-  justify-content: space-between;
+	@mixin headerFlex {
+		display: flex;
+		flex-wrap: nowrap;
+		align-content: center;
+		align-items: center;
+	}
+	height: 60px;
+	position: fixed;
+	left: 0;
+	right: 0;
+	z-index: 1;
+	border-bottom: 1px solid $border-color;
+	@include headerFlex;
+	color: $text-color;
+	justify-content: space-between;
 
-  .header-left,.header-right {
-    @include headerFlex;
-  }
+	.header-left, .header-right {
+		@include headerFlex;
+	}
 
-  .logo {
-    font-size: 22px;
-    line-height: 60px;
-    width: 200px;
-    padding-left: 30px;
+	.logo {
+		font-size: 22px;
+		line-height: 60px;
+		width: 200px;
+		padding-left: 30px;
 
-  }
+	}
 
-  .header-nav {
-    display: flex;
-    flex-wrap: nowrap;
-    margin-right: 20px;
+	.header-nav {
+		display: flex;
+		flex-wrap: nowrap;
+		margin-right: 20px;
 
-    .header-nav-item {
-      width: 60px;
-      text-align: center;
+		.header-nav-item {
+			width: 60px;
+			text-align: center;
 
-      a {
-        @include aActive;
-      }
-    }
-  }
+			a {
+				@include aActive;
+			}
+		}
+	}
 
-  .header-right {
+	.header-right {
 
-  }
+	}
 }
 </style>
--- examples/router/index.js
+++ examples/router/index.js
@@ -2,6 +2,7 @@ import VueRouter from "vue-router";
 import Vue from "vue";
 import routes from "../routes.config";
 import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const baseSiteName = 'Try UI';
 Vue.use(VueRouter)
@@ -10,33 +11,34 @@ Vue.use(VueRouter)
  * @param {String} title 即时更新标题
  */
 function changeTitle(title) {
-    if (title) {
-        document.title = title;
-    } else {
-        document.title = process.env.VUE_APP_TITLE;
-    }
+	if (title) {
+		document.title = title;
+	} else {
+		document.title = process.env.VUE_APP_TITLE;
+	}
 }
 
-NProgress.configure({showSpinner: false}); // NProgress Configuration
-
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes
 });
+
+NProgress.configure({showSpinner: false}); // NProgress Configuration
 
 router.changeTitle = changeTitle;
 /**
  * 路由权限拦截
  */
 router.beforeEach((to, from, next) => {
-    NProgress.start()
-    changeTitle(to.meta.title || baseSiteName)
-    next();
+	NProgress.start()
+	changeTitle(to.meta.title || baseSiteName)
+	next();
+
 });
 
 router.afterEach((to) => {
-    NProgress.done();
+	NProgress.done();
 });
 
 export default router;
