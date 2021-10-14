25c32fdd778ebf1643e0d7406b6b0f4af34b533c
iyanghong <iyanghong@foxmail.com>
2021-10-14 20:57:59 +0800

    新增url-loader加载

--- build/webpack.demo.js
+++ build/webpack.demo.js
@@ -11,79 +11,89 @@ const isProd = process.env.NODE_ENV === 'production';
 const MarkdownLoader = require('./md-loader')
 
 module.exports = {
-    entry :  './examples/index.js',
-    devServer : {
-        hot: true,
-        host: 'localhost',
-        port: '6660',
-        contentBase : './examples/dist',
-        historyApiFallback : true
-    },
-    resolve: {
-        extensions: ['.js', '.vue', '.json'],
-        alias: {
-            '@' : path.resolve(process.cwd(),'./examples')
-        }
-    },
-    output: {
-        path: path.resolve(process.cwd(), './examples/dist/'),
-        publicPath: process.env.CI_ENV || '/',
-        filename: '[name].[hash:7].js',
-        chunkFilename: isProd ? '[name].[hash:7].js' : '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.(vue|jsx?)$/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader'
-            },
-            {
-                test: /\.(jsx?|babel|es6)$/,
-                include: process.cwd(),
-                exclude: config.jsexclude,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    compilerOptions: {
-                        preserveWhitespace: false
-                    }
-                }
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                    { loader: 'sass-resources-loader',
-                        options: {
-                            sourceMap: true,
-                            resources:[path.resolve(__dirname, '../src/theme/common/variables.scss'),]//一定是path.resolve的绝对路径
-                        }
-                    }
-                ]
-            },
-            MarkdownLoader
-        ]
-    },
-    plugins:[
-        new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            template: './examples/index.tpl',
-            filename: './index.html',
-            favicon: './examples/favicon.ico'
-        }),
-        new webpack.LoaderOptionsPlugin({
-            vue: {
-                compilerOptions: {
-                    preserveWhitespace: false
-                }
-            }
-        })
-    ],
+	entry: './examples/index.js',
+	devServer: {
+		hot: true,
+		host: 'localhost',
+		port: '6660',
+		contentBase: './examples/dist',
+		historyApiFallback: true
+	},
+	resolve: {
+		extensions: ['.js', '.vue', '.json'],
+		alias: {
+			'@': path.resolve(process.cwd(), './examples')
+		}
+	},
+	output: {
+		path: path.resolve(process.cwd(), './examples/dist/'),
+		publicPath: process.env.CI_ENV || '/',
+		filename: '[name].[hash:7].js',
+		chunkFilename: isProd ? '[name].[hash:7].js' : '[name].js'
+	},
+	module: {
+		rules: [
+			{
+				enforce: 'pre',
+				test: /\.(vue|jsx?)$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader'
+			},
+			{
+				test: /\.(jsx?|babel|es6)$/,
+				include: process.cwd(),
+				exclude: config.jsexclude,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {
+					compilerOptions: {
+						preserveWhitespace: false
+					}
+				}
+			},
+			{
+				test: /\.(scss|css)$/,
+				use: [
+					isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+					'css-loader',
+					'sass-loader',
+					{
+						loader: 'sass-resources-loader',
+						options: {
+							sourceMap: true,
+							resources: [path.resolve(__dirname, '../src/theme/common/variables.scss'),]//一定是path.resolve的绝对路径
+						}
+					}
+				]
+			},
+			{
+				test: /\.(svg|otf|ttf|woff2?|eot|gif|png|jpe?g)(\?\S*)?$/,
+				loader: 'url-loader',
+				// todo: 这种写法有待调整
+				query: {
+					limit: 10000,
+					name: path.posix.join('static', '[name].[hash:7].[ext]')
+				}
+			},
+			MarkdownLoader
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin(),
+		new HtmlWebpackPlugin({
+			template: './examples/index.tpl',
+			filename: './index.html',
+			favicon: './examples/favicon.ico'
+		}),
+		new webpack.LoaderOptionsPlugin({
+			vue: {
+				compilerOptions: {
+					preserveWhitespace: false
+				}
+			}
+		})
+	],
 }
