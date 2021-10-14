9765a08e67266af1a74ec9532117a02096ab9626
iyanghong <iyanghong@foxmail.com>
2021-10-14 20:58:30 +0800

    修复css导入错误问题

--- examples/index.js
+++ examples/index.js
@@ -6,17 +6,20 @@ import TryUi from '../src/index';
 import SideNav from './layout/side-nav'
 import MainHeader from './layout/header';
 import Search from './layout/search'
-import '../src/theme/input.scss';
+import icons from './icons.json';
+import '../src/theme/index.scss';
 import './styles/index.scss';
 import 'github-markdown-css/github-markdown.css'
+
 console.log(TryUi)
 Vue.component('markdown-demo-render-block', MarkdownDemoRender)
 Vue.component('side-nav', SideNav)
 Vue.component('main-header', MainHeader)
 Vue.component('Search', Search)
 Vue.use(TryUi);
+Vue.prototype.$icons = icons;
 
 new Vue({
-    router,
-    render: h => h(App)
+	router,
+	render: h => h(App)
 }).$mount('#app')
