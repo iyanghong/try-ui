0d5be89f29877056f139b7febca002caf88aecf6
iyanghong <iyanghong@foxmail.com>
2021-10-17 12:18:09 +0800

    修改传参类型

--- src/utils/util.js
+++ src/utils/util.js
@@ -89,7 +89,7 @@ export const EventListener = {
 	/**
 	 * Listen to DOM events during the bubble phase.
 	 *
-	 * @param {DOMEventTarget} target DOM element to register listener on.
+	 * @param {DOMEventTarget|Element} target DOM element to register listener on.
 	 * @param {string} eventType Event type, e.g. 'click' or 'mouseover'.
 	 * @param {function} callback Callback function.
 	 * @return {object} Object with a `remove` method.
