86247fb75b075387bfa9b0fe35273acf6a827617
iyanghong <iyanghong@foxmail.com>
2021-10-22 20:37:59 +0800

    修改examples内容块为t-scrollbar

--- examples/layout/index.vue
+++ examples/layout/index.vue
@@ -1,63 +1,64 @@
 <template>
-	<div class="examples-layout">
-		<main-header></main-header>
-		<side-nav :data="navList"></side-nav>
-		<div :class="['container', isMarkdown ? 'markdown-body' : '',componentsClass]">
-			<router-view></router-view>
-		</div>
-	</div>
+    <div class="examples-layout">
+        <main-header></main-header>
+        <side-nav :data="navList"></side-nav>
+        <t-scrollbar :class="['container', isMarkdown ? 'markdown-body' : '',componentsClass]">
+            <router-view></router-view>
+        </t-scrollbar>
+    </div>
 </template>
 
 <script>
 import NavConfig from '../nav.config.json'
 
 export default {
-	name: "index",
-	data() {
-		return {
-			navList: NavConfig['zh-CN']
-		}
-	},
-	computed: {
-		isMarkdown() {
-			return this.$route.meta.isMarkdown;
-		},
-		componentsClass() {
-			return 'demo-' + this.$route.path.slice(1).replace('/', '-') + '-body';
-		}
-	},
-	created() {
-	},
-	methods: {}
+    name: "index",
+    data() {
+        return {
+            navList: NavConfig['zh-CN']
+        }
+    },
+    computed: {
+        isMarkdown() {
+            return this.$route.meta.isMarkdown;
+        },
+        componentsClass() {
+            return 'demo-' + this.$route.path.slice(1).replace('/', '-') + '-body';
+        }
+    },
+    created() {
+    },
+    methods: {}
 
 }
 </script>
 
 <style lang="scss">
 * {
-	padding: 0;
-	margin: 0;
+    padding: 0;
+    margin: 0;
 }
 
 .examples-layout {
-	display: flex;
-	flex-wrap: nowrap;
+    display: flex;
+    flex-wrap: nowrap;
 
-	h1, h2, h3, h4, h5, h6 {
-		font-weight: 400;
-		color: #1f2f3d;
-	}
+    h1, h2, h3, h4, h5, h6 {
+        font-weight: 400;
+        color: #1f2f3d;
+    }
 
-	.side-nav {
-		top: 61px;
-	}
+    .side-nav {
+        top: 61px;
+    }
 
-	.container {
-		width: 100%;
-		margin-left: 200px;
-		margin-top: 60px;
-		padding: 20px;
-		box-sizing: border-box;
-	}
+    .container {
+        width: calc(100vw - 200px);
+        height: calc(100vh - 60px);
+        margin-left: 200px;
+        margin-top: 60px;
+        padding: 20px;
+        box-sizing: border-box;
+    }
 }
 </style>
