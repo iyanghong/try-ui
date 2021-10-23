8718bdff3042e45f73f65dc4fa60c845edc27bf7
iyanghong <iyanghong@foxmail.com>
2021-10-23 15:39:46 +0800

    新增环境变量

--- build/config.js
+++ build/config.js
@@ -2,9 +2,11 @@ exports.jsexclude = /node_modules|utils\/popper\.js|utils\/date\.js/;
 
 exports.env = {
     production: {
-        PUBLIC_PATH: '"/try-ui/"'
+        PUBLIC_PATH: '"/try-ui/"',
+        DEFAULT_ROUTER: '"component-layout"'
     },
     development: {
-        PUBLIC_PATH: '"/"'
+        PUBLIC_PATH: '"/"',
+        DEFAULT_ROUTER: '"component-layout"'
     }
 }
