18fb8674bcbdd228f17c321180eaaa2552c9ccef
iyanghong <iyanghong@foxmail.com>
2021-10-25 21:23:47 +0800

    新增基本Select

---
+++ src/components/select/index.js
@@ -0,0 +1,7 @@
+import TSelect from './src/select.vue';
+
+TSelect.install = (Vue) => {
+    Vue.component(TSelect.name, TSelect);
+};
+
+export default TSelect;
---
+++ src/components/select/src/select.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="t-select" :class="size ? 't-select--' + size : ''" @click.stop="handleClick"
+         v-click-out-side="handleClose">
+        <span class="t-select--value" v-text="value"></span>
+        <input
+            type="hidden"
+            ref="selectValue"
+            v-bind="$attrs"
+        >
+        <span class="t-select--icon" :class="isDropdown ? 'is-dropdown' : ''"><i
+            class="t-icon-list-arrow-down"></i></span>
+
+        <options-layer :visible="isDropdown">
+            <ul class="t-select--options">
+                <template v-if="items">
+                    <t-option
+                        v-for="(item,index) in items"
+                        :class="item.value === value ? 'is-selected' : ''"
+                        :key="index"
+                        :value="item.value">
+                        {{ item.label || item.value }}
+                    </t-option>
+                </template>
+            </ul>
+        </options-layer>
+    </div>
+</template>
+
+<script>
+import TOption from './option.vue';
+import ClickOutSide from '../../../utils/click-out-side.js';
+import Dispatch from '../../../mixins/dispatch.js'
+import OptionsLayer from './options-layer.vue';
+
+export default {
+    name: 'TSelect',
+    props: {
+        size: {
+            type: String,
+            default: ''
+        },
+        value: {
+            required: true
+        },
+        items: Array
+    },
+    components: {
+        TOption,
+        OptionsLayer
+    },
+    mixins: [
+        Dispatch
+    ],
+    directives: {ClickOutSide},
+    data() {
+        return {
+            isDropdown: false,
+            inputWidth: 0
+        }
+    },
+    created() {
+        this.resetInputWidth();
+    },
+    methods: {
+        handleClick() {
+            this.isDropdown = !this.isDropdown;
+        },
+        handleClose() {
+            this.isDropdown = false;
+        },
+        handleSetSelect(value) {
+            this.$emit('input', value);
+            this.$refs.selectValue.value = value;
+        },
+        resetInputWidth() {
+            this.$nextTick(() => {
+                this.inputWidth = this.$el.getBoundingClientRect().width;
+            });
+        }
+    }
+}
+</script>
