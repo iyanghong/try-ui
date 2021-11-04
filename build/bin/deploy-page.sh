b17b0d629614ce2ab5665641f95358cd58308865
iyanghong <iyanghong@foxmail.com>
2021-11-04 18:02:16 +0800

    新增GitHub Page快速打包并提交更新快捷命令

--- build/bin/deploy-page.sh
+++ build/bin/deploy-page.sh
@@ -1,1 +1,19 @@
-echo 1;
+rm -rf ./examples/dist;
+mkdir ./examples/dist;
+
+cd ./examples/dist;
+git init;
+git remote add try-ui git@github.com:iyanghong/try-ui.git;
+git pull try-ui page;
+git checkout page;
+git rm -r *;
+
+npm run build:example;
+
+
+## 由于GitHub Page不支持伪静态，即不支持vue-router的history模式。所以复制index.html文件为404.html，让GitHub Page找不到的时候自动重定向至404.html
+cp ./index.html ./404.html;
+
+git add . && git add . && git commit -m "update page";
+
+git push try-ui page;
